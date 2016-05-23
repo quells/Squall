9eb619160cf2b91ac11b113b5ff03c5c88e4a732
Kai Wells <support@kaiwells.me>
2016-05-23 12:26:37 -0700

    Swift 3.0 Changes

--- Sources/Squall.swift
+++ Sources/Squall.swift
@@ -18,7 +18,7 @@ public enum Squall {
     /// Initialize the internal state of the generator.
     ///
     /// - parameter seed: The value used to generate the intial state. Should be chosen at random.
-    public static func seed(seed: UInt32) {
+    public static func seed(_ seed: UInt32) {
         Squall.generator = MersenneTwisterGenerator(seed: seed)
     }
     
@@ -63,27 +63,25 @@ extension Squall {
         // Finding a contiguous stretch of memory much longer than this might not be possible, especially on iOS
         precondition(length < 10485760, "Buffer would exceed 10mb")
         
-        let rawMemory = UnsafeMutablePointer<Void>.alloc(length)
-        guard rawMemory != nil else { return nil }
+        let rawMemory = UnsafeMutablePointer<Void>.allocatingCapacity(length)
+        // guard rawMemory != nil else { return nil }
         let buffer = UnsafeMutablePointer<UInt8>(rawMemory)
         
         let numChunks = length / 4
         for index in 0..<numChunks {
-            let ptr = buffer + (index * 4)
             let chunk = Squall.random() as UInt32
-            ptr.memory               = UInt8((chunk & 0xFF000000) >> 24)
-            ptr.advancedBy(1).memory = UInt8((chunk & 0x00FF0000) >> 16)
-            ptr.advancedBy(2).memory = UInt8((chunk & 0x0000FF00) >> 8)
-            ptr.advancedBy(3).memory = UInt8((chunk & 0x000000FF))
+            (buffer + (index * 4)).pointee     = UInt8((chunk & 0xFF000000) >> 24)
+            (buffer + (index * 4) + 1).pointee = UInt8((chunk & 0x00FF0000) >> 16)
+            (buffer + (index * 4) + 2).pointee = UInt8((chunk & 0x0000FF00) >> 8)
+            (buffer + (index * 4) + 3).pointee = UInt8((chunk & 0x000000FF))
         }
         
         var numExtra = length % 4
         let extraChunk = Squall.random() as UInt32
-        let ptr = buffer + (numChunks * 4)
         while numExtra > 0 {
             let shift = UInt32(numExtra * 8)
             let mask: UInt32 = 0xFF << shift
-            ptr.advancedBy(numExtra).memory = UInt8((extraChunk & mask) >> shift)
+            (buffer + (numChunks * 4) + numExtra).pointee = UInt8((extraChunk & mask) >> shift)
             numExtra -= 1
         }
         
@@ -91,8 +89,8 @@ extension Squall {
             bytesNoCopy: rawMemory,
             length: length,
             deallocator: { (ptr, length) in
-                ptr.destroy(length)
-                ptr.dealloc(length)
+                ptr.deinitialize(count: length)
+                ptr.deallocateCapacity(length)
             }
         )
     }
