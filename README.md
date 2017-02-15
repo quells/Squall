90b46fad8da579cc950ef529cf5f267624b169d0
Kai Wells <support@kaiwells.me>
2017-02-14 17:28:29 -0800

    GeneratorType => IteratorProtocol Removed UInt64 and randomData()

--- README.md
+++ README.md
@@ -11,9 +11,19 @@ Implementation of the MT algorithm for 32-bit integers.
 Wrapper around the MT implementation that provides random values for several types:
 
 - UInt32
-- UInt64
 - Float
 - Double
-- NSData
 
 Provides both uniform and Gaussian (normal) distributions for Float and Double.
+
+## Changelog
+
+### 1.1 - February 14, 2017
+
+- Updated for Swift 3.1
+- Removed `Squall.random() -> UInt64`
+- Removed `Squall.randomData()` because the syntax for `UnsafeMutablePointer` changed.
+
+### 1.0 - May 21, 2016
+
+- Initial Release
--- Sources/Squall.swift
+++ Sources/Squall.swift
@@ -13,15 +13,15 @@ import Foundation
 /// CAUTION: Not thread safe, uses a global internal state.
 ///
 public enum Squall {
-    private static var generator = MersenneTwisterGenerator()
-    
+    internal static var generator = MersenneTwisterGenerator()
+
     /// Initialize the internal state of the generator.
     ///
     /// - parameter seed: The value used to generate the intial state. Should be chosen at random.
     public static func seed(_ seed: UInt32) {
         Squall.generator = MersenneTwisterGenerator(seed: seed)
     }
-    
+
     /// Initialize the internal state of the generator.
     ///
     /// Uses the current time as the seed. Should not overflow until February 2106.
@@ -32,67 +32,12 @@ public enum Squall {
         let seed = UInt32(epochTime)
         Squall.seed(seed)
     }
-    
+
     /// Generates a random `UInt32`.
     ///
     /// - returns: The next `UInt32` in the sequence
     public static func random() -> UInt32 {
         return generator.next()!
-    }
-}
-
-// MARK: Extended byte sequences
-
-extension Squall {
-    /// Generates a random `UInt64`.
-    ///
-    /// - returns: The next two `UInt32`'s in the sequence concatenated into a `UInt64`
-    public static func random() -> UInt64 {
-        let upper = UInt64(generator.next()!) << 32
-        let lower = UInt64(generator.next()!)
-        return upper + lower
-    }
-    
-    /// Generates a random stream of bytes.
-    ///
-    /// - parameter length: The number of bytes of random data to return.
-    ///
-    /// - returns: The next `length` random bytes. Or nil if length <= 0.
-    public static func randomData(length: Int) -> NSData? {
-        guard length > 0 else { return nil }
-        // Finding a contiguous stretch of memory much longer than this might not be possible, especially on iOS
-        precondition(length < 10485760, "Buffer would exceed 10mb")
-        
-        let rawMemory = UnsafeMutablePointer<Void>.allocatingCapacity(length)
-        // guard rawMemory != nil else { return nil }
-        let buffer = UnsafeMutablePointer<UInt8>(rawMemory)
-        
-        let numChunks = length / 4
-        for index in 0..<numChunks {
-            let chunk = Squall.random() as UInt32
-            (buffer + (index * 4)).pointee     = UInt8((chunk & 0xFF000000) >> 24)
-            (buffer + (index * 4) + 1).pointee = UInt8((chunk & 0x00FF0000) >> 16)
-            (buffer + (index * 4) + 2).pointee = UInt8((chunk & 0x0000FF00) >> 8)
-            (buffer + (index * 4) + 3).pointee = UInt8((chunk & 0x000000FF))
-        }
-        
-        var numExtra = length % 4
-        let extraChunk = Squall.random() as UInt32
-        while numExtra > 0 {
-            let shift = UInt32(numExtra * 8)
-            let mask: UInt32 = 0xFF << shift
-            (buffer + (numChunks * 4) + numExtra).pointee = UInt8((extraChunk & mask) >> shift)
-            numExtra -= 1
-        }
-        
-        return NSData(
-            bytesNoCopy: rawMemory,
-            length: length,
-            deallocator: { (ptr, length) in
-                ptr.deinitialize(count: length)
-                ptr.deallocateCapacity(length)
-            }
-        )
     }
 }
 
@@ -111,7 +56,7 @@ extension Squall {
         let d = upper - lower
         return f*d + lower
     }
-    
+
     /// Pull from a uniform distribution of random numbers. Defaults to [0, 1).
     ///
     /// - parameter lower: Lower bound of uniform distribution. Defaults to 0.
@@ -142,7 +87,7 @@ extension Squall {
         let X = sqrt(-2*log(A))*cos(2*M_PI*B)
         return X*sigma + mu
     }
-    
+
     /// Pull from a Gaussian distribution of random numbers.
     ///
     /// - parameter average: The center of the distribution. Defaults to 0.
